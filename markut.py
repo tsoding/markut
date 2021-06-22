7424233b8eb29f8c3c3f3ef8eab765556b8a54eb
rexim <reximkut@gmail.com>
2021-06-22 17:57:10 +0700

    Add basic types to markut

--- markut.py
+++ markut.py
@@ -5,15 +5,21 @@ import sys
 import subprocess
 import argparse
 
-def ts_to_secs(ts):
+from collections.abc import Iterable
+from typing import List
+
+def ts_to_secs(ts: str) -> int:
     comps = ts.split(':');
     assert len(comps) == 3;
     return 60 * 60 * int(comps[0]) + 60 * int(comps[1]) + int(comps[2]);
 
-def secs_to_ts(secs):
+def secs_to_ts(secs: int) -> str:
     return f'{secs//60//60:02}:{secs//60%60:02}:{secs%60:02}';
 
-def ffmpeg_cut_chunk(input_path, start_secs, duration_secs, output_path):
+def ffmpeg_cut_chunk(input_path: str,
+                     start_secs: int,
+                     duration_secs: int,
+                     output_path: str) -> None:
     cli = ['ffmpeg',
            '-ss', str(start_secs),
            '-i', input_path,
@@ -22,7 +28,7 @@ def ffmpeg_cut_chunk(input_path, start_secs, duration_secs, output_path):
            output_path];
     subprocess.run(cli)
 
-def ffmpeg_concat_chunks(list_path, output_path):
+def ffmpeg_concat_chunks(list_path: str, output_path: str) -> None:
     cli = ['ffmpeg',
            '-f', 'concat',
            '-safe', '0',
@@ -31,12 +37,12 @@ def ffmpeg_concat_chunks(list_path, output_path):
            output_path]
     subprocess.run(cli)
 
-def ffmpeg_generate_concat_list(chunk_names, output_path):
+def ffmpeg_generate_concat_list(chunk_names: Iterable[str], output_path: str) -> None:
     with open(output_path, 'w') as f:
         for name in chunk_names:
             f.write(f"file '{name}'\n")
 
-def load_ts_from_file(path, delay):
+def load_ts_from_file(path: str, delay: int) -> List[int]:
     with open(path, newline='') as csvfile:
         return [ts_to_secs(row[0]) + delay
                 for row in csv.reader(csvfile)];
@@ -53,12 +59,12 @@ if __name__ == '__main__':
     assert n % 2 == 0
 
     secs = 0;
-    cuts_ts = [];
+    cuts_ts: List[str] = [];
     for i in range(0, n // 2):
         start    = ts[i * 2 + 0]
         end      = ts[i * 2 + 1]
         duration = end - start
-        secs    += dur
+        secs    += duration
         cuts_ts.append(secs_to_ts(secs));
         ffmpeg_cut_chunk(args.input, start, duration, f'chunk-{i:02}.mp4')
 
@@ -68,5 +74,5 @@ if __name__ == '__main__':
     ffmpeg_concat_chunks(ourlist_path, "output.mp4")
 
     print("Timestamps of cuts:");
-    for ts in cuts_ts:
-        print(ts)
+    for cut in cuts_ts:
+        print(cut)
